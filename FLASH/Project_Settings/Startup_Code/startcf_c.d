a53ea5b35ddcc3c654273f3354e313118d20476b
chalsRemoteServer <chals24x@gmail.com>
2026-05-07 13:09:36 -0600

    7 may 109pm

--- FLASH/Project_Settings/Startup_Code/startcf_c.d
+++ FLASH/Project_Settings/Startup_Code/startcf_c.d
@@ -1,11 +1,11 @@
-Project_Settings/Startup_Code/startcf_c.obj: D:\00_Desarrollo1_MIS_DOCs\Documents\02_DESARROLLO\04_FIRMWARE\03_GSI_ASD+_f\03_uP_f\v41.1_IN3UP\40_04_01_IN3UP\Project_Settings\Startup_Code\startcf.c \
+Project_Settings/Startup_Code/startcf_c.obj: D:\00_Desarrollo1_MIS_DOCs\Documents\02_DESARROLLO\04_FIRMWARE\03_GSI_ASD+_f\03_uP_f\v41.1_IN3UP\40_04_05_IN3UP\Project_Settings\Startup_Code\startcf.c \
 	C:\Freescale\CW\ MCU\ v10.7\MCU\ColdFire_Support\ewl\EWL_C\include\lib_ewl.prefix \
 	C:\Freescale\CW\ MCU\ v10.7\MCU\ColdFire_Support\ewl\EWL_C\include\common.prefix \
 	C:\Freescale\CW\ MCU\ v10.7\MCU\ColdFire_Support\ewl\EWL_C\include\ewl_c_version.h \
-	D:\00_Desarrollo1_MIS_DOCs\Documents\02_DESARROLLO\04_FIRMWARE\03_GSI_ASD+_f\03_uP_f\v41.1_IN3UP\40_04_01_IN3UP\Project_Settings\Startup_Code\startcf.h \
-	D:\00_Desarrollo1_MIS_DOCs\Documents\02_DESARROLLO\04_FIRMWARE\03_GSI_ASD+_f\03_uP_f\v41.1_IN3UP\40_04_01_IN3UP\Project_Headers\support_common.h \
+	D:\00_Desarrollo1_MIS_DOCs\Documents\02_DESARROLLO\04_FIRMWARE\03_GSI_ASD+_f\03_uP_f\v41.1_IN3UP\40_04_05_IN3UP\Project_Settings\Startup_Code\startcf.h \
+	D:\00_Desarrollo1_MIS_DOCs\Documents\02_DESARROLLO\04_FIRMWARE\03_GSI_ASD+_f\03_uP_f\v41.1_IN3UP\40_04_05_IN3UP\Project_Headers\support_common.h \
 	C:\Freescale\CW\ MCU\ v10.7\MCU\ProcessorExpert\lib\MCF\iofiles\MCF52233.h \
-	D:\00_Desarrollo1_MIS_DOCs\Documents\02_DESARROLLO\04_FIRMWARE\03_GSI_ASD+_f\03_uP_f\v41.1_IN3UP\40_04_01_IN3UP\Generated_Code\PE_Types.h \
+	D:\00_Desarrollo1_MIS_DOCs\Documents\02_DESARROLLO\04_FIRMWARE\03_GSI_ASD+_f\03_uP_f\v41.1_IN3UP\40_04_05_IN3UP\Generated_Code\PE_Types.h \
 	C:\Freescale\CW\ MCU\ v10.7\MCU\ColdFire_Support\ewl\EWL_C\include\stdint.h \
 	C:\Freescale\CW\ MCU\ v10.7\MCU\ColdFire_Support\ewl\EWL_C\include\ansi_parms.h \
 	C:\Freescale\CW\ MCU\ v10.7\MCU\ColdFire_Support\ewl\EWL_C\include\ewlGlobals.h \
--- FLASH/Sources/TFTmenus_c.d
+++ FLASH/Sources/TFTmenus_c.d
@@ -1,10 +1,10 @@
-Sources/TFTmenus_c.obj: D:\00_Desarrollo1_MIS_DOCs\Documents\02_DESARROLLO\04_FIRMWARE\03_GSI_ASD+_f\03_uP_f\v41.1_IN3UP\40_04_01_IN3UP\Sources\TFTmenus.c \
+Sources/TFTmenus_c.obj: D:\00_Desarrollo1_MIS_DOCs\Documents\02_DESARROLLO\04_FIRMWARE\03_GSI_ASD+_f\03_uP_f\v41.1_IN3UP\40_04_05_IN3UP\Sources\TFTmenus.c \
 	C:\Freescale\CW\ MCU\ v10.7\MCU\ColdFire_Support\ewl\EWL_C\include\lib_ewl.prefix \
 	C:\Freescale\CW\ MCU\ v10.7\MCU\ColdFire_Support\ewl\EWL_C\include\common.prefix \
 	C:\Freescale\CW\ MCU\ v10.7\MCU\ColdFire_Support\ewl\EWL_C\include\ewl_c_version.h \
-	D:\00_Desarrollo1_MIS_DOCs\Documents\02_DESARROLLO\04_FIRMWARE\03_GSI_ASD+_f\03_uP_f\v41.1_IN3UP\40_04_01_IN3UP\Project_Headers\TFTmenus.h \
-	D:\00_Desarrollo1_MIS_DOCs\Documents\02_DESARROLLO\04_FIRMWARE\03_GSI_ASD+_f\03_uP_f\v41.1_IN3UP\40_04_01_IN3UP\Project_Headers\IOcomms.h \
-	D:\00_Desarrollo1_MIS_DOCs\Documents\02_DESARROLLO\04_FIRMWARE\03_GSI_ASD+_f\03_uP_f\v41.1_IN3UP\40_04_01_IN3UP\Generated_Code\PE_Types.h \
+	D:\00_Desarrollo1_MIS_DOCs\Documents\02_DESARROLLO\04_FIRMWARE\03_GSI_ASD+_f\03_uP_f\v41.1_IN3UP\40_04_05_IN3UP\Project_Headers\TFTmenus.h \
+	D:\00_Desarrollo1_MIS_DOCs\Documents\02_DESARROLLO\04_FIRMWARE\03_GSI_ASD+_f\03_uP_f\v41.1_IN3UP\40_04_05_IN3UP\Project_Headers\IOcomms.h \
+	D:\00_Desarrollo1_MIS_DOCs\Documents\02_DESARROLLO\04_FIRMWARE\03_GSI_ASD+_f\03_uP_f\v41.1_IN3UP\40_04_05_IN3UP\Generated_Code\PE_Types.h \
 	C:\Freescale\CW\ MCU\ v10.7\MCU\ColdFire_Support\ewl\EWL_C\include\stdint.h \
 	C:\Freescale\CW\ MCU\ v10.7\MCU\ColdFire_Support\ewl\EWL_C\include\ansi_parms.h \
 	C:\Freescale\CW\ MCU\ v10.7\MCU\ColdFire_Support\ewl\EWL_C\include\ewlGlobals.h \
@@ -14,6 +14,6 @@ Sources/TFTmenus_c.obj: D:\00_Desarrollo1_MIS_DOCs\Documents\02_DESARROLLO\04_FI
 	C:\Freescale\CW\ MCU\ v10.7\MCU\ColdFire_Support\ewl\EWL_C\include\limits_api.h \
 	C:\Freescale\CW\ MCU\ v10.7\MCU\ColdFire_Support\ewl\EWL_C\include\ewl_lib_ext1.h \
 	C:\Freescale\CW\ MCU\ v10.7\MCU\ColdFire_Support\ewl\EWL_C\include\ewl_misra_types.h \
-	D:\00_Desarrollo1_MIS_DOCs\Documents\02_DESARROLLO\04_FIRMWARE\03_GSI_ASD+_f\03_uP_f\v41.1_IN3UP\40_04_01_IN3UP\Project_Headers\TFTmenu.h \
-	D:\00_Desarrollo1_MIS_DOCs\Documents\02_DESARROLLO\04_FIRMWARE\03_GSI_ASD+_f\03_uP_f\v41.1_IN3UP\40_04_01_IN3UP\Project_Headers\queue.h \
-	D:\00_Desarrollo1_MIS_DOCs\Documents\02_DESARROLLO\04_FIRMWARE\03_GSI_ASD+_f\03_uP_f\v41.1_IN3UP\40_04_01_IN3UP\Project_Headers\system.h 
+	D:\00_Desarrollo1_MIS_DOCs\Documents\02_DESARROLLO\04_FIRMWARE\03_GSI_ASD+_f\03_uP_f\v41.1_IN3UP\40_04_05_IN3UP\Project_Headers\TFTmenu.h \
+	D:\00_Desarrollo1_MIS_DOCs\Documents\02_DESARROLLO\04_FIRMWARE\03_GSI_ASD+_f\03_uP_f\v41.1_IN3UP\40_04_05_IN3UP\Project_Headers\queue.h \
+	D:\00_Desarrollo1_MIS_DOCs\Documents\02_DESARROLLO\04_FIRMWARE\03_GSI_ASD+_f\03_uP_f\v41.1_IN3UP\40_04_05_IN3UP\Project_Headers\system.h 
